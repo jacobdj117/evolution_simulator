ac985f3b62d3c00042a8fd7ae634c4cc3b74d0f7
Jacob <jacobdj117@gmail.com>
2025-02-04 08:30:23 -0600

    hotfix: adds changes missing from previous merge

--- include/logger.h
+++ include/logger.h
@@ -12,9 +12,9 @@ class Logger {
 private:
     const std::string file_name_ = "log.txt";
 
-    std::unique_ptr<World> world_;
-    std::ofstream file_;
     uint16_t count_;
+    std::ofstream file_;
+    std::unique_ptr<World> world_;
 
     void log_point(uint16_t x, uint16_t y);
 
