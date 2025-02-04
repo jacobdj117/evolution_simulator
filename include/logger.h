d3ff36231ad75965521ba706f8e2bca25940e81f
Jacob <jacobdj117@gmail.com>
2025-02-04 08:09:55 -0600

    Adds file handling

--- include/logger.h
+++ include/logger.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <fstream>
 #include <memory>
+#include <string>
 
 #include "world.h"
 
@@ -8,11 +10,16 @@ namespace evo_sim {
 
 class Logger {
 private:
+    const std::string file_name_ = "log.txt";
+
     std::unique_ptr<World> world_;
+    std::ofstream file_;
 
 public:
     Logger(World* world_ref);
-    ~Logger() = default;
+    ~Logger();
+
+    void log_world_state();
 
 }; // End class Logger
 
--- src/logger.cpp
+++ src/logger.cpp
@@ -2,4 +2,15 @@
 
 evo_sim::Logger::Logger(World* world_ref)
     : world_ {world_ref}
-{ }
+    , file_ {file_name_}
+{
+    if (!file_.is_open()) throw "could not open output file";
+}
+
+evo_sim::Logger::~Logger() {
+    if (file_.is_open()) file_.close();
+}
+
+void evo_sim::Logger::log_world_state() {
+
+}
