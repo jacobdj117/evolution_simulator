713aaeb9c53463203858dbb5af6aa5f977efa659
Jacob <jacobdj117@gmail.com>
2025-02-10 14:11:26 -0600

    first pass at random number class

--- include/evo_random.h
+++ include/evo_random.h
@@ -8,12 +8,16 @@ namespace evo_sim {
 
 class Evo_Random {
 private:
-    
+    std::random_device device_;
+    std::mt19937 gen_;
+    point world_end_;
+
 public:
-    Evo_Random();
-    Evo_Random(int init_seed);
+    Evo_Random(point init_world_end);
     ~Evo_Random() = default;
 
+    uint16_t random_between(uint16_t low, uint16_t high);
+    float random_between(float low, float high);
     point random_point();
 
 }; // End class Evo_Random
--- src/evo_random.cpp
+++ src/evo_random.cpp
@@ -1,13 +1,23 @@
 #include "evo_random.h"
 
-evo_sim::Evo_Random::Evo_Random() {
+evo_sim::Evo_Random::Evo_Random(point init_world_end)
+    : device_ {}
+    , gen_ {device_()}
+    , world_end_ {init_world_end}
+{}
 
+uint16_t evo_sim::Evo_Random::random_between(uint16_t low, uint16_t high) {
+    std::uniform_int_distribution<> distrib(low, high);
+    return distrib(gen_);
 }
 
-evo_sim::Evo_Random::Evo_Random(int init_seed) {
-
+float evo_sim::Evo_Random::random_between(float low, float high) {
+    std::uniform_real_distribution<> distrib(low, high);
+    return distrib(gen_);
 }
 
 evo_sim::point evo_sim::Evo_Random::random_point() {
-    return {0, 0};
+    uint16_t x = random_between(0, world_end_.first);
+    uint16_t y = random_between(0, world_end_.second);
+    return {x, y};
 }
